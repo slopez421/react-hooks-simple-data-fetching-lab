31a8a2e4acd42d5cb588d26252f3ab93a2c7beaf
Shania Lopez <slopez@fandm.edu>
2023-07-02 13:42:12 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App () {
+const [dog, setDog] = useState("");
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((image) => setDog(image.message))
+}, [])
+
+if(!dog) {
+    return <h2>Loading...</h2>
+}
+return <p>
+    <img src={dog} alt="A Random Dog" />
+</p>
+}
+
+export default App;
